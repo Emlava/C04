a227b6a14a0984974eb7e3e86ca81d886221bad0
Emlava <emiliolaravargas@hotmail.com>
2025-02-18 13:58:20 +0100

    added ex02

---
+++ ex02/ft_putnbr.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+		write(1, "-2147483648", 11);
+	else if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar('-');
+		ft_putnbr(nb);
+	}
+	else if (nb < 10)
+		ft_putchar(nb + '0');
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+}
+
+/*int	main()
+{
+	int nb = -2147483648;
+	
+	ft_putnbr(nb);
+	write(1, "\n", 1);
+	return (0);
+}*/
